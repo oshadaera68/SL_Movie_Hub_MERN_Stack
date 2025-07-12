729ab38e1e6a186dfe5771668f08e30e8e6a4aa0
oshadaera68 <mindartlk010@gmail.com>
2025-07-12 09:12:46 +0530

    updated content

--- frontend/src/components/BrowseMovies/BrowseMovies.jsx
+++ frontend/src/components/BrowseMovies/BrowseMovies.jsx
@@ -7,5 +7,6 @@ export default function BrowseMovies() {
     return (<>
             <h1>Developing soon...</h1>
             <h2>  &copy; 2025 SL Movies Hub. All rights reserved.</h2>
+        test
         </>)
 }
